1137d76d7a6aba904fe6a4a175b8bda99c929c15
Katia <Katia@Katia-PC>
2017-09-04 18:04:04 -0500

    version

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Imprime ejemplo 'Versión actual de PHP: 5.3.8'
+echo 'Versión actual de PHP: ' . phpversion();
+
+// Imprime ejemplo '2.0' o nada si la extensión no está habilitada
+echo phpversion('tidy');
+?>
